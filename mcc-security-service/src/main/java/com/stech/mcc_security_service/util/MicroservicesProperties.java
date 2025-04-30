09acb464844c26593eee5c7daa59857a746bec78
FRANK <frankmascco@gmail.com>
2025-04-30 09:03:56 -0500

    add security microservice

---
+++ mcc-security-service/src/main/java/com/stech/mcc_security_service/util/MicroservicesProperties.java
@@ -0,0 +1,22 @@
+package com.stech.mcc_security_service.util;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "mcc-security")
+public class MicroservicesProperties {
+
+    List<Application> applications;
+
+    @Data
+    public static class Application{
+
+        private String clientId;
+        private String clientSecret;
+    }
+}
